13c25afb5d1929a78ed2561fe87f8d0b273681bc
Victoria <93212653+vikkisnax@users.noreply.github.com>
2022-12-14 20:29:30 -0500

    done with MVP and styled buttons (#1)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+const rootElement = document.getElementById("root");
+
+// You'll need to wrap <App /> for routing to work
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, rootElement);
+
+
+//so App will render the components and show them on screen
